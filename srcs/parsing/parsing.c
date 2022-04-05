1d3e7a0874f08c814c1213e452b5e148431eb75f
Olivier Cartier <me@cestoliv.com>
2022-04-05 16:28:38 +0200

    reql solution for the previous problem + comments

--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -6,12 +6,20 @@
 /*   By: ocartier <ocartier@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 09:08:48 by ocartier          #+#    #+#             */
-/*   Updated: 2022/04/05 14:55:42 by ocartier         ###   ########.fr       */
+/*   Updated: 2022/04/05 16:27:16 by ocartier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
+/*
+	Return the end of the first arg of given str
+	It's gonna find the end index of the arg after the quotes
+		(it takes the closing quote index as parameter)
+	It return the index of the first space except if that space
+	is after a &&, ||, |, <<, <, >> or >, it's gonna return the
+	index just before that characters
+*/
 int	get_arg_end(char *str, int quote_index)
 {
 	char	**splitchar;
@@ -28,29 +36,50 @@ int	get_arg_end(char *str, int quote_index)
 	{
 		test_index = index_of(str + quote_index, splitchar[cur], 1);
 		if (test_index < end_index)
-			return (test_index + quote_index + strarr_free(splitchar));
+		{
+			strarr_free(splitchar);
+			return (test_index + quote_index);
+		}
 	}
-	return (end_index + quote_index + strarr_free(splitchar));
+	strarr_free(splitchar);
+	return (end_index + quote_index);
 }
 
+/*
+	Return the end of the first arg of given str
+	It's like a wrapper for get_arg_end() that jump directly to the closing quotes
+	It return get_arg_end() with the index of the closing quote
+		(if the arg starts with quotes, index_of need to find the first quote,
+		if it doesn't start with quotes but contains quotes,
+			index_of need to find the second quote, because the first is the opening quote
+	Return -2 if there is not any quotes
+*/
 int	get_quotes_end(char *str, int e_end)
 {
 	int	end_index;
 
 	end_index = -2;
 	if (str[0] == '\'')
-		end_index = index_of(str + 1, "'", 1) + 2;
+		end_index = get_arg_end(str, index_of(str + 1, "'", 1) + 2);
 	else if (e_end > -1 && index_of(str, "'", 1) < e_end)
 		end_index = get_arg_end(str, index_of(str + 1, "'", 2) + 2);
 	else if (str[0] == '"')
-		end_index = index_of(str + 1, "\"", 1) + 2;
+		end_index = get_arg_end(str, index_of(str + 1, "\"", 1) + 2);
 	else if (e_end > -1 && index_of(str, "\"", 1) < e_end)
 		end_index = get_arg_end(str, index_of(str + 1, "\"", 2) + 2);
-	if (end_index != -2 && str[end_index] != ' ')
-		end_index += index_of(str + end_index, " ", 1);
 	return (end_index);
 }
 
+/*
+	Return the end of the first arg of given str
+	e_end is equal to the return of get_arg_end
+		on the current string (with a quote_index = 0)
+	A e_end = to -1 means that str start with a special char
+		like quotes, &, |, < or >
+	It can find if the arg is actually longer than the e_end
+		because of some quotes (with get_quotes_end)
+	or if it's lower because of a &&, ||, |, <<, <, >> or > before
+*/
 int	get_end_index(char *str, int e_end)
 {
 	int		end_index;
@@ -78,6 +107,12 @@ int	get_end_index(char *str, int e_end)
 	return (e_end + strarr_free(splitchar));
 }
 
+/*
+	Split the given command in the given args list
+	Works like a split on spaces, but it also split on <, <<, >, >>, &&, |, ||
+	Of course, it doesn't split on these characters if they are surrounded
+	by simple or double quotes
+*/
 int	split_args(t_list **args, char *cmd)
 {
 	int		cur;
