3196302b452c03ecec4a7c131235bf00d8b02b79
Olivier Cartier <me@cestoliv.com>
2022-04-13 10:51:02 +0200

    add a check_format for export

--- srcs/exec/export.c
+++ srcs/exec/export.c
@@ -1,17 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   export.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ocartier <ocartier@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/13 10:50:09 by ocartier          #+#    #+#             */
+/*   Updated: 2022/04/13 10:50:45 by ocartier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/minishell.h"
 
-int     is_egal(char *s)
+/*
+	Print the error corresponding to the given error code
+*/
+static int	print_error(int error_code, char *content)
 {
-    int i;
+	if (error_code == 1)
+	{
+		ft_printf("\033[91m%s '%c'\033[0m\n",
+			"export: name cannot start with", *content);
+	}
+	else if (error_code == 2)
+	{
+		ft_printf("\033[91m%s '%c'\033[0m\n",
+			"export: not valid in this context:", *content);
+	}
+	return (1);
+}
 
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == '=')
-            return 1;
-        i++;
-    }
-    return 0;
+/*
+	Return 1 on syntax error, 0 if there is no error
+	Error :	The env name starts with a number
+			The env name contains other chars than [A-Z][a-z][0-9]_
+			The arg doesn't contain an equal sign
+	Display an error with ft_printf when the error is not the missing equal sign
+
+	E.g.:	1name=oliv
+			na-me=oliv
+			name = oliv
+*/
+int	check_format(char	*arg)
+{
+	int	cur;
+	int	has_equal;
+
+	if (!ft_isalpha(arg[0]))
+		return (print_error(1, &arg[0]));
+	cur = 0;
+	has_equal = 0;
+	while (arg[cur])
+	{
+		if (arg[cur] == '=')
+			has_equal = 1;
+		else if (!ft_isalnum(arg[cur]) && arg[cur] != '_')
+			if (!has_equal)
+				return (print_error(2, &arg[cur]));
+		cur++;
+	}
+	if (!has_equal)
+		return (1);
+	return (0);
 }
 
 char    **ft_export(t_mem *mem, char *s)
@@ -21,7 +71,7 @@ char    **ft_export(t_mem *mem, char *s)
     int i;
 
     i = 0;
-    if (!is_egal(s))
+    if (check_format(s))
         return mem->my_env;
     temp = ft_split(s, '=');
     if (is_in_env(mem->my_env, temp[0]))
